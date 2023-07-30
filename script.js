7c417b39213707ec0dcb1bcd42279551051ffd96
aryan26-01 <104006434+aryan26-01@users.noreply.github.com>
2023-07-30 22:09:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("myButton").addEventListener("click",function(){
+    document.getElementById("my-form").reset();
+    alert("Details Submitted! You will be contacted soon");
+});
